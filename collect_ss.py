03671d4015616f035655cefbc6014c2edb6d87e8
qjlxg <qjlxg@163.com>
2025-03-15 11:43:09 +0800

    Update collect_ss.py

--- collect_ss.py
+++ collect_ss.py
@@ -20,7 +20,7 @@ def is_valid_url(url):
         return False
 
 # 获取原始URL列表
-sub_all_clash_url = 'https://github.com/qjlxg/ss/raw/refs/heads/master/sources.list'
+sub_all_clash_url = 'https://github.com/qjlxg/collectSub/raw/refs/heads/main/sub/sub_all_clash.txt'
 try:
     response = requests.get(sub_all_clash_url, headers=headers, timeout=10)
     response.raise_for_status()
