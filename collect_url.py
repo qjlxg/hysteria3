5492f927dab2ff930c54f0ccdc9857deded53224
qjlxg <qjlxg@163.com>
2025-04-02 17:53:29 +0800

    Update collect_url.py

--- collect_url.py
+++ collect_url.py
@@ -21,7 +21,7 @@ def is_valid_url(url):
         return False
 
 # 获取原始URL列表
-sub_all_clash_url = 'https://raw.githubusercontent.com/qjlxg/TV/refs/heads/main/2022-2024_url'
+sub_all_clash_url = 'https://jdss.hilaotie.me/'
 try:
     response = requests.get(sub_all_clash_url, headers=headers, timeout=10)
     response.raise_for_status()
