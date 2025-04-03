a5e88b48c31d18dcaece942cbd85f0ac2172787b
qjlxg <qjlxg@163.com>
2025-04-03 08:39:24 +0800

    Update 2022-2024.py

--- 2022-2024.py
+++ 2022-2024.py
@@ -21,7 +21,7 @@ def is_valid_url(url):
         return False
 
 # 获取原始URL列表
-sub_all_clash_url = 'https://raw.githubusercontent.com/qjlxg/TV/refs/heads/main/2022-2024_url'
+sub_all_clash_url = 'https://raw.githubusercontent.com/qjlxg/hysteria3/refs/heads/main/202209-202210.txt'
 try:
     response = requests.get(sub_all_clash_url, headers=headers, timeout=10)
     response.raise_for_status()
