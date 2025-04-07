61eb72d2d993aa08106b8ec7492919d2f7711c54
qjlxg <qjlxg@163.com>
2025-04-07 11:59:31 +0800

    Update all.py

--- all.py
+++ all.py
@@ -21,7 +21,7 @@ def is_valid_url(url):
         return False
 
 # 获取原始URL列表
-sub_all_clash_url = 'https://rxsub.dedyn.io/sub/sources/sub_info.txt'
+sub_all_clash_url = 'https://github.com/qjlxg/hysteria3/raw/refs/heads/main/url'
 try:
     response = requests.get(sub_all_clash_url, headers=headers, timeout=10)
     response.raise_for_status()
