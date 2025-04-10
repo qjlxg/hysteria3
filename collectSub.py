121f861bd87f49df2ec2a2f1ebe288c618722007
qjlxg <qjlxg@163.com>
2025-04-10 10:57:21 +0800

    Update collectSub.py

--- collectSub.py
+++ collectSub.py
@@ -8,7 +8,8 @@ headers = {
 }
 
 # 配置参数
-TIMEOUT = 15      # 单次请求超时时间（秒）
+MAX_SUCCESS = 99999  # 需要获取的有效内容数量
+TIMEOUT = 256      # 单次请求超时时间（秒）
 OUTPUT_FILE = 'collectSub.txt'
 
 def is_valid_url(url):
